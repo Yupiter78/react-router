852861ccfd310b7a5d4107c82c6674a669b055e5
Yriy Yupiter <yu.piter.gm@gmail.com>
2021-09-19 03:11:54 +0300

    react-router Route_2

---
+++ src/app/components/navBar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+
+const NavBar = () => {
+    // ul>(li>a)*4
+    return (
+        <ul>
+            <li>
+                <a href="/">Home</a>
+            </li>
+            <li>
+                <a href="/login">Login</a>
+            </li>
+            <li>
+                <a href="/posts">Posts</a>
+            </li>
+            <li>
+                <a href="/dashboard">Dashboard</a>
+            </li>
+        </ul>
+    );
+};
+
+export default NavBar;
