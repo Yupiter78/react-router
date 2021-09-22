fcac3a2c8e12ed5cc8915a27c442d5b7c4c8e861
yuriy_yupiter <yu.piter.gm@gmail.com>
2021-09-22 21:20:43 +0300

    react-router lodash & Query-params_4

--- src/app/components/posts.jsx
+++ src/app/components/posts.jsx
@@ -2,26 +2,34 @@ import React from "react";
 import PropTypes from "prop-types";
 import Post from "./Post";
 import PostsList from "./postsList";
+import query from "query-string";
+import _ from "lodash";
 
-const Posts = ({ match }) => {
+const Posts = ({ match, location }) => {
     const posts = [
         { id: 1, label: "post 1" },
         { id: 2, label: "post 2" },
         { id: 3, label: "post 3" }
     ];
+    const search = query.parse(location.search);
     const postId = match.params.postId;
+    console.log("search:", search);
+    const cropPosts = search
+        ? _(posts).slice(0).take(search.count).value()
+        : posts;
     return (
         <>
             {postId ? (
                 <Post id={postId} posts={posts} />
             ) : (
-                <PostsList posts={posts} />
+                <PostsList posts={cropPosts} />
             )}
         </>
     );
 };
 Posts.propTypes = {
-    match: PropTypes.object.isRequired
+    match: PropTypes.object.isRequired,
+    location: PropTypes.object.isRequired
 };
 
 export default Posts;
